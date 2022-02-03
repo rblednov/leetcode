6fb50a82791afa755a1f8e6d9187a2ed7625790d
Roman Blednov <rblednov@alfabank.ru>
2022-02-03 19:47:37 +0300

    Two Sum

---
+++ src/main/java/ru/rblednov/leetcode/arrays/FourSum.java
@@ -0,0 +1,11 @@
+package ru.rblednov.leetcode.arrays;
+
+public class FourSum {
+    public static int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
