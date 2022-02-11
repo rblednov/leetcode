cf7f95b2269be0e93a7683a823e5276844ade23c
Roman Blednov <rblednov@alfabank.ru>
2022-02-11 18:07:28 +0300

    .

--- src/main/java/ru/rblednov/leetcode/arrays/SubarraySumEqualsK560.java
+++ src/main/java/ru/rblednov/leetcode/arrays/SubarraySumEqualsK560.java
@@ -1,23 +1,27 @@
 package ru.rblednov.leetcode.arrays;
 
-public class SubarraySumEqualsK560 {
-    class Solution {
-        public int subarraySum(int[] nums, int k) {
-            int lb=0;
-            int rb=0;
-            int sum=0;
-            int response =0 ;
-            while(true){
-                if(sum<k){
-                    if(rb<nums.length){
-                        rb=rb+1;
-                        sum = sum+nums[rb];
-                    }
-                    return 0;
-                } else {
+import java.util.HashMap;
+import java.util.Map;
 
-                }
-            }
+public class SubarraySumEqualsK560 {
+    // 1 1 2 -3 1 2 -2 1 0 3 // 3
+    // 1 2 4  1 2 4  2 3 3 6 // 3
+
+    public static int subarraySum(int[] nums, int k) {
+        int sum = 0;
+        int response = 0;
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        hashMap.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum + nums[i];
+            response = response + hashMap.getOrDefault(sum - k, 0);
+            hashMap.put(sum, hashMap.getOrDefault(sum, 0) + 1);
         }
+        return response;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(subarraySum(new int[]{1, -1, 0}, 0));
     }
 }
